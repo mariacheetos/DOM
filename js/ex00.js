98c507c1690ef62c4b0ae33f4548579b3c836849
mariacheetos <maria.clara.santos.ifpr@gmail.com>
2025-10-20 16:52:54 -0300

    add

---
+++ js/ex00.js
@@ -0,0 +1,26 @@
+/**Exemplo 01 - getElementById */
+var elemento = document.getElementById("titulo")
+alert(elemento)
+console.log(elemento)
+
+/*Exemplo 02 - innerHTML */
+function mudarTitulo() {
+    var elemento = document.getElementById("titulo")
+    elemento.innerHTML = "Manipulando o título via JS"
+    var elemento02 = document.getElementById("titulo02")
+    elemento02.innerHTML = "Maria Luísa chata"
+}
+/*Exemplo 03 - mudar estilo */
+function mudarEstilo() {
+    var elemento
+    elemento = document.getElementById("titulo")
+    elemento.style.color = "blue"
+    elemento.style.backgroundColor = "pink"
+    elemento.style.fontSize = "70px"
+}
+
+/*Exemplo 04 - classList.add  localiza o elemento desejado e insere uma classe HTML*/
+function destacar(){
+    var elemento = document.getElementById("titulo02")
+    elemento.classList.add("destaque")
+}
